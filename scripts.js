ff8e59ae87971cbe749fb5520ed59c1b8f949cb8
nbkhope <ryumemaru@yahoo.com>
2016-09-19 18:02:39 -0700

    Add initial Backbone Views

--- scripts.js
+++ scripts.js
@@ -19,5 +19,39 @@ console.log(otherBlog.toJSON());
 // Backbone collection
 var Blogs = Backbone.Collection.extend({});
 
-
 var blogs = new Blogs([blog, otherBlog]);
+
+// Backbone views
+
+// view for one blog
+var BlogView = Backbone.View.extend({
+  model: new Blog(),
+  tagName: 'tr', // this will be referred as $el
+  initialize: function() {
+    var blogsListTemplate = $('#blogs-list-template').html();
+    // uses underscore template
+    this.template = _.template(blogsListTemplate);
+  },
+  render: function() {
+    this.$el.html(
+      this.template({ model: this.model.toJSON() })
+    );
+  }
+});
+
+// view for all blogs
+var BlogsView = Backbone.View.extend({
+  model: blogs,
+  el: $('.blogs-list'),
+  initialize: function() {
+    this.model.on('add', this.render(), this);
+  },
+  render: function() {
+    var self = this;
+    this.$el.html('');
+    _.each(this.model, function(blog) {
+      var newBlogView = new BlogView({ model: blog });
+      self.$el.append(newBlogView.render().$el);
+    });
+  }
+});
