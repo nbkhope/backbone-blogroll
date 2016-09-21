aec6a3e9c2a96576962ce187267b4f6de63845ec
nbkhope <ryumemaru@yahoo.com>
2016-09-20 20:15:21 -0700

    Add server code to connect to mongodb using mongoose

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 // Import modules
 var express = require('express');
+var mongoose = require('mongoose');
 
 // Make the app
 var app = express();
+
+// Connect to the Mongo DB
+mongoose.connect('mongodb://localhost/backboneblogroll');
+
+// Create DB Schema
+var Schema = mongoose.Schema;
+var BlogSchema = new Schema({
+  author: String,
+  title: String,
+  url: String
+});
+
+// Create model based on the Schema given
+mongoose.model('Blog', BlogSchema);
+var Blog = mongoose.model('Blog');
+
+// add new entry to the db
+var myBlog = new Blog({
+  author: 'Marty Cat',
+  title: 'Marty\'s Cat Blog',
+  url: 'http://www.github.com/'
+});
+
+myBlog.save();
 
 // Set up simple static server at /public
 app.use(express.static(__dirname + '/public'));
